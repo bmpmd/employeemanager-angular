12553754e28c027bbfaed1e1d9ee62278b84b3bf
leo juarez <sk8leo@live.com>
2022-12-09 15:41:52 -0500

    cleaned up ui

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatLegacySliderModule as MatSliderModule } from '@angular/material/legacy-slider';
-
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCommonModule } from '@angular/material/core';
+import { MatCardModule } from '@angular/material/card';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
@@ -16,8 +25,18 @@ import { MatLegacySliderModule as MatSliderModule } from '@angular/material/lega
     BrowserModule,
     HttpClientModule,
     BrowserAnimationsModule,
-   
-
+    MatSlideToggleModule,
+    MatCommonModule,
+    MatCardModule,
+    MatDialogModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatListModule,
+    MatIconModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatInputModule,
+    
 
 
 
