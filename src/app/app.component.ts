86d4e253427d0972fdf36b829dd19f83870e5858
leo juarez <sk8leo@live.com>
2022-12-15 14:55:42 -0500

    added delete dialog, not functional yet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,42 @@
+import { MatDialog, MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { HttpClient } from '@angular/common/http';
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { Employee } from './model/employee';
 import { EmployeeService } from './service/employee.service';
 import { HttpErrorResponse } from '@angular/common/http';
- 
+
+export interface DeleteDialogData{
+  id: number, 
+  name: string
+}
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
+
 export class AppComponent implements OnInit {
   title = 'employeemanager-angular';
 
 
   //place to hold employees list all the time 
   public employees: Employee[] | undefined;
-  constructor(private employeeService: EmployeeService) { }
+  constructor(private employeeService: EmployeeService, public dialog: MatDialog) { }
 
+  openDeleteDialog(empid: number, empname: string){
+    const dialogRef = this.dialog.open(DeleteDialog,{
+      data: {id:empid, name: empname}
+    });
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`delete dialog result: ${result}`);
+    })
+  }
+  
   ngOnInit(){
     this.getEmployees();
   }
@@ -37,3 +57,19 @@ export class AppComponent implements OnInit {
   }
 
 }
+
+@Component({
+  selector: 'delete-dialog',
+  templateUrl: 'delete-dialog.html',
+})
+export class DeleteDialog {
+  constructor(
+    public dialogRef: MatDialogRef<DeleteDialog>, 
+    @Inject(MAT_DIALOG_DATA) public data: DeleteDialogData,
+  ){}
+
+  onNoClick():void{
+    this.dialogRef.close();
+  }
+}
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HttpClientModule } from '@angular/common/http';
-import { AppComponent } from './app.component';
+import { AppComponent, DeleteDialog } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatCommonModule } from '@angular/material/core';
@@ -19,7 +19,8 @@ import {MatInputModule} from '@angular/material/input';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DeleteDialog
   ],
   imports: [
     BrowserModule,
